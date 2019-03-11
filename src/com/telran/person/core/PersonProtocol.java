6cfc29b70800bf4d02c4355dcccfc783452c5606
Anton Konkin <konkin.anton@gmail.com>
2019-03-11 15:50:20 +0200

    Personal Protocol. Data structure was changed

--- src/com/telran/person/core/PersonProtocol.java
+++ src/com/telran/person/core/PersonProtocol.java
@@ -13,11 +13,11 @@ import java.util.function.Function;
 
 public class PersonProtocol implements Protocol {
     private Map<String, Function<ProtocolRequest, ProtocolResponse>> mapper;
-    private List<Person> persons;
+    private Map<Integer, Person> persons;
 
     public PersonProtocol() {
         mapper = new HashMap<>();
-        persons = new ArrayList<>();
+        persons = new HashMap<>();
         mapper.put(PersonApi.RequestType.ADD_PERSON, this::addPerson);
         mapper.put(PersonApi.RequestType.REMOVE_PERSON, this::removePerson);
         mapper.put(PersonApi.RequestType.GET_BY_ID, this::getPersonById);
@@ -41,39 +41,28 @@ public class PersonProtocol implements Protocol {
 
     private ProtocolResponse addPerson(ProtocolRequest request) {
         Person person = (Person) request.data;
-        if (person == null || indexOf(person.getId()) != null) {
+        if (person == null || persons.containsKey(person.getId())) {
             return new ProtocolResponse(ProtocolResponse.Code.WRONG_REQUEST, false);
         }
-        persons.add(person);
+        persons.put(person.getId(),person);
         return new ProtocolResponse(ProtocolResponse.Code.OK, true);
     }
 
     private ProtocolResponse removePerson(ProtocolRequest request) {
         int id = (int) request.data;
-        Person toFind = indexOf(id);
+        Person toFind = persons.remove(id);
         if (toFind == null) {
             return new ProtocolResponse(ProtocolResponse.Code.WRONG_REQUEST, false);
         }
-        persons.remove(toFind);
         return new ProtocolResponse(ProtocolResponse.Code.OK, true);
     }
 
     private ProtocolResponse getPersonById(ProtocolRequest request) {
         int id = (int) request.data;
-        Person toFind = indexOf(id);
+        Person toFind = persons.get(id);
         if (toFind == null) {
             return new ProtocolResponse(ProtocolResponse.Code.WRONG_REQUEST, toFind);
         }
         return new ProtocolResponse(ProtocolResponse.Code.OK, toFind);
-    }
-
-    private Person indexOf(int personId) {
-        if (personId < 0) {
-            return null;
-        }
-        return persons.stream()
-                .filter(person -> person.getId() == personId)
-                .findFirst().orElse(null);
-
     }
 }
